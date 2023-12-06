9556795488999b12b0dd9514c5058a16da0011e8
Allen-Wei-1123 <46612547+Allen-Wei-1123@users.noreply.github.com>
2023-12-06 13:55:09 -0800

    commit

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -9,6 +9,8 @@ import { newUserSchema } from '@/lib/zod';
 import { dropUser, getNumberOfUsers, storeUser } from '@/lib/storage';
 import { createProfile, newPrivateKey } from '@/lib/contract';
 import LoginWindow from './loginWindow';
+import { Server, User } from 'lucide-react';
+import { User as newUser } from '@/lib/types';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -57,7 +59,7 @@ export default async function RootLayout({
     /*
       TODO #1: Indicate that this function is a server function by adding 'use server';
     */
-
+    'use server'
     /*
       TODO #2: Create the new User object with a username, name, and privateKey
     
@@ -65,13 +67,22 @@ export default async function RootLayout({
         - 
         - Use the newPrivateKey() function to generate a new private key for the user
     */
-
+    const privatekey = newPrivateKey();
+    const newUser:  newUser = {
+      username: form.values.name,
+      name : form.values.name,
+      privateKey: privatekey,
+      followers : 0,
+      following:0
+    };
+   
+    
     /* 
       TODO #3: Store the user in the local account cache
 
       HINT: Use the storeUser() function to store the user
     */
-
+    await storeUser(newUser);
     /* 
       TODO #4: Set up a try catch block to create the user's profile and log them in if successful.
 
@@ -83,6 +94,14 @@ export default async function RootLayout({
           from the local account cache. Then, throw the error to be caught by the catch block in
           the loginWindow.tsx file.
     */
+   try{
+      await createProfile(newUser);
+      await login(newUser);
+   }catch(error:any){
+      await dropUser(newUser);
+      // return error;
+      throw new Error(error.message);
+    }
   }
 
   if (!me) {
--- app/app/loginWindow.tsx
+++ app/app/loginWindow.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from "react";
+import { any } from "zod";
 
 export default function LoginWindow(
   { setUpProfile }: {
@@ -11,7 +12,7 @@ export default function LoginWindow(
   /* 
     TODO #1: Add a state variable to store the current error message
   */
-
+  const [errorMessage, setErrorMessage] = useState<string|null>(null);
   /*
     This function is called to set up the profile of the new user. It is called
     when a user submits the login form. It takes a FormData object as an argument,
@@ -22,7 +23,7 @@ export default function LoginWindow(
     /* 
       TODO #3: Set the error state to an empty string
     */
-
+    setErrorMessage("");
     /* 
       TODO #4: Set up a try catch block to call the setUpProfile() function and set the error state
       if an error is thrown
@@ -31,6 +32,12 @@ export default function LoginWindow(
         - Use the setUpProfile() function to set up the user's profile and log them in
         - In the catch block, set the error state to the error message (error.message)
     */
+    try {
+      await setUpProfile(form)
+    } catch (error:any) {
+        console.log("error is "+error.message)
+        setErrorMessage(error.message);
+    }
   }
 
   return (
@@ -65,7 +72,9 @@ export default function LoginWindow(
           <p className="text-red-500">
             {/* 
               TODO #2: Display the error message if it is not an empty string using the error state variable
-            */}
+            */
+              errorMessage
+            }
             {
               "PLACEHOLDER"
             }
